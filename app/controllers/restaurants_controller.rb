83c639351c80676b413f750563d366b9aeb76f13
Temi Adaramewa <miss.t.adaramewa@gmail.com>
2022-01-20 21:38:22 +0000

    controllers and routes added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,43 @@
 class RestaurantsController < ApplicationController
-  
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+   # @restaurant = Restaurant.new(restaurant_params)
+    # @restaurant.save
+    # redirect_to restaurant_path(@restaurant)
+  # end
+
+  @restaurant = Restaurant.new(restaurant_params)
+  if @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  else
+    render :new
+  end
 end
+
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end 
+end 
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end 
